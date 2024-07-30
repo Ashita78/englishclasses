6ec3954d5062cece0ab3df7555229ae5790cd577
Yaneth Montaña <japamosi@yahoo.es>
2024-07-30 06:44:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-hi 
+ 
+Here you can introduce yourself
+import pandas as pd
+import numpy as np
+
+dataset = pd.DataFrame([
+ ("Mexico", "Married", "High school"),
+ ("Colombia", "Single", "Undergraduate"),
+ ("Guinea Equatorial", "Divorced", "College"),
+ ("Mexico", "Single", "Primary"),
+ ("Colombia", "Single", "Primary"),
+], columns=["Country", "Marital status", "Education" ])
